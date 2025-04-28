2419c38e931f6ada14b822f25a5781f05b111da9
Muhammad Nauman <nauman33183@gmail.com>
2025-04-28 12:00:45 +0500

    railway setup

--- client/src/utils/exports.jsx
+++ client/src/utils/exports.jsx
@@ -1,2 +1,3 @@
 // export const backendURL = "https://factory-project-backend-lac657oo6.vercel.app/api"
-export const backendURL = "http://localhost:5000/api"
+// export const backendURL = "http://localhost:5000/api"
+export const backendURL = "http://artistic-acceptance-production.up.railway.app/api"
