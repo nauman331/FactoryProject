f4a615ca3bf75af51bdc9b461d3ed27788605032
Muhammad Nauman <nauman33183@gmail.com>
2025-05-06 20:15:10 +0500

    unique JobID

--- server/utils/generateTaskId.js
+++ server/utils/generateTaskId.js
@@ -1,6 +1,19 @@
-const generateTaskId = () => {
-  const base36 = Date.now().toString(36).toUpperCase().slice(-6);
-  return `AI-${base36}`;
+const Product = require('../models/Job'); // Adjust path as needed
+
+const generateUniqueTaskId = async () => {
+  let isUnique = false;
+  let JobId;
+
+  while (!isUnique) {
+    const sixDigit = Math.floor(100000 + Math.random() * 900000);
+    taskId = `AI-${sixDigit}`;
+    const existing = await Product.findOne({ JobId });
+    if (!existing) {
+      isUnique = true;
+    }
+  }
+
+  return JobId;
 };
 
-module.exports = generateTaskId;
+module.exports = generateUniqueTaskId;
